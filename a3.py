ebbcbe41166a2a0da0fcb33c4da5966e9727a7be
coder0304 <nellikatgowri@gmail.com>
2025-01-19 12:15:27 +0530

    a3.py

---
+++ a3.py
@@ -0,0 +1,7 @@
+import math 
+print("The floor and ceiling  alue of 23.56 are: "+str(math.ceil(23.56))+','+ str(math.floor(23.56)))
+x=10
+y=-15
+print("the value of x after copying the sign from y is: "+ str(math.copysign(x,y)))
+print("Absolute value of -96 and 56 are: ",(math.fabs(-96),',',(math.fabs(56))))
+print("thegcd of 24 and 56 " +str(math.gcd(24,56)))
